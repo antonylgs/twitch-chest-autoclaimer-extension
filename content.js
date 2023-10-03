ccd5e0dd1d017b532e2baebfd45b9256b66f7fb9
antonylgs <antonylanglois@outlook.com>
2023-10-03 17:37:54 +0200

    V1.1 - claim while chat folded

--- content.js
+++ content.js
@@ -1,4 +1,6 @@
-let checkForBonusChestOnFirstLoad = false;
+let firstCheckOnObserverMode = false;
+let claimingMethod = "interval";
+let observer;
 
 // Function which tries to click the "Claim Bonus" button if it is present
 function clickClaimBonusButtonIfPresent() {
@@ -6,37 +8,38 @@ function clickClaimBonusButtonIfPresent() {
   if (claimButton) {
     claimButton.click();
     console.log("[Twitch Bonus Chest Auto Claimer] Chest claimed!");
-    setTimeout(() => {
-      setupObserver('[aria-label="Claim Bonus"]');
-    }, 5000);
+
+    if (claimingMethod == "observer") {
+      setTimeout(() => {
+        setupObserver('[aria-label="Claim Bonus"]');
+      }, 5000);
+    }
 
     chrome.storage.local.get("claimedBonusChestCount", function (result) {
       const currentCount = result.claimedBonusChestCount || 0;
       chrome.storage.local.set({ claimedBonusChestCount: currentCount + 1 });
     });
-  } else if (!checkForBonusChestOnFirstLoad) {
-    // If the bonus chest button is not avalaible on the first load, set up the observer on the bonus chest parent node
+  } else if (firstCheckOnObserverMode) {
     console.log(
-      "[Twitch Bonus Chest Auto Claimer] Bonus chest parent node found! Looking for the chest button..."
+      "[Twitch Bonus Chest Auto Claimer] No chest found during the first observer load, starting the observer for bonus chest"
     );
     setupObserver('[aria-label="Claim Bonus"]');
+  } else {
+    console.log(
+      "[Twitch Bonus Chest Auto Claimer] Bonus chest not found! Trying again in 30 seconds..."
+    );
   }
 }
 
 // Function to be executed when the target element is found
 function onElementPresent(targetSelector) {
-  if (targetSelector === '[data-test-selector="community-points-summary"]') {
-    if (!checkForBonusChestOnFirstLoad) {
-      // On first page load a chest might be already available, so try to click it
-      clickClaimBonusButtonIfPresent();
-      checkForBonusChestOnFirstLoad = true;
-    } else {
-      // If the target element is the bonus chest parent node, set up the observer on the bonus chest button
-      console.log(
-        "[Twitch Bonus Chest Auto Claimer] Bonus chest parent node found! Looking for the chest button..."
-      );
-      setupObserver('[aria-label="Claim Bonus"]');
-    }
+  if (
+    targetSelector === '[data-test-selector="community-points-summary"]' &&
+    firstCheckOnObserverMode
+  ) {
+    // On first page load a chest might be already available, so try to click it
+    clickClaimBonusButtonIfPresent();
+    firstCheckOnObserverMode = false;
   } else if (targetSelector === '[aria-label="Claim Bonus"]') {
     // If the target element is the bonus chest button, try to click it
     console.log(
@@ -54,7 +57,7 @@ function setupObserver(targetSelector) {
     onElementPresent(targetSelector);
   } else {
     // Set up a MutationObserver to watch for changes in the target node
-    const observer = new MutationObserver(function (mutationsList, observer) {
+    observer = new MutationObserver(function (mutationsList, observer) {
       for (const mutation of mutationsList) {
         if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
           // Check if the target element is now present
@@ -85,7 +88,61 @@ function setupObserver(targetSelector) {
   }
 }
 
-console.log(
-  "[Twitch Bonus Chest Auto Claimer] Twitch bonus chest claimer loaded! Looking for the parentNode..."
-);
-setupObserver('[data-test-selector="community-points-summary"]');
+function changeClaimingMethod(method) {
+  if (method == "observer") {
+    firstCheckOnObserverMode = true;
+    console.log(
+      "[Twitch Bonus Chest Auto Claimer] Twitch bonus chest claimer loaded! Looking for the parentNode..."
+    );
+    setupObserver('[data-test-selector="community-points-summary"]');
+    clearInterval(intervalId);
+    console.log("[Twitch Bonus Chest Auto Claimer] Interval cleared");
+  } else if (method == "interval") {
+    if (observer) {
+      observer.disconnect();
+      console.log("[Twitch Bonus Chest Auto Claimer] Observer disconnected");
+    }
+    clickClaimBonusButtonIfPresent();
+    console.log("[Twitch Bonus Chest Auto Claimer] Interval set");
+    intervalId = setInterval(clickClaimBonusButtonIfPresent, 30000);
+  }
+}
+
+// Function to check the class of the chat-shell element
+function checkChatShellClass() {
+  const chatShell = document.querySelector(".chat-shell");
+  if (chatShell) {
+    if (chatShell.classList.contains("chat-shell__expanded")) {
+      console.log(
+        "[Twitch Bonus Chest Auto Claimer] Changing the claiming method to observer"
+      );
+      claimingMethod = "observer";
+      changeClaimingMethod(claimingMethod);
+    } else {
+      console.log(
+        "[Twitch Bonus Chest Auto Claimer] Changing the claiming method to interval"
+      );
+      claimingMethod = "interval";
+      changeClaimingMethod(claimingMethod);
+    }
+  }
+}
+
+// Function to be called when a mutation is observed
+function handleMutation(mutationsList) {
+  for (const mutation of mutationsList) {
+    if (mutation.type === "attributes" && mutation.attributeName === "class") {
+      checkChatShellClass();
+    }
+  }
+}
+
+// Observe the chat-shell element for changes
+const chatShell = document.querySelector(".chat-shell");
+if (chatShell) {
+  const observer = new MutationObserver(handleMutation);
+  observer.observe(chatShell, { attributes: true });
+}
+
+// Check the class when the page initially loads
+window.addEventListener("load", checkChatShellClass);
